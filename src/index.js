f7a5e36a818ff11aab9c7d3cb382208c32966b57
Panganiban Miguel <24022674@myrp.edu.sg>
2025-10-24 13:38:31 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom/client';
 function App(){
     return <>
     <Header />
-    <Pizza name="" image="" price="" description="" />
+    <Pizza name="Focaccia" image="\pizzas\focaccia.jpg" price="10" description="test1" />
+    <Pizza name="Funghi" image="\pizzas\funghi.jpg" price="10" description="test2" />
+    <Pizza name="Margherita" image="\pizzas\margherita.jpg" price="10" description="test3" />
+    <Pizza name="Prosciutto" image="\pizzas\prosciutto.jpg" price="10" description="test4" />
+    <Pizza name="Salamino" image="\pizzas\salamino.jpg" price="10" description="test5" />
+    <Pizza name="Spinaci" image="\pizzas\spinaci.jpg" price="10" description="test6" />
     </>;
 }
 
@@ -13,9 +18,12 @@ function Header(){
 
 }
 
-function Pizza(name,image,price,description){
+function Pizza({name,image,price,description}){
     return <>
-    
+    <h2>${price}</h2>
+    <img src={image} alt={name}/>
+    <h4>{name}</h4>
+    <h4>{description}</h4>
     </>
 }
 
